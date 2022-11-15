db43d3ef02731289ae6cc76bf47cbc6dadcd119c
Snow Cube <snow.cube@outlook.com>
2022-11-15 18:51:55 +0800

    layout test

--- scripts/main.js
+++ scripts/main.js
@@ -1,9 +1,14 @@
+// Basic game object, which store kinds of info of game
 var game = {
-    size: 3,
-    piecesSize: 0,
-    pieces: [],
+    size: 3, // size * size puzzle
+    pcsSize: 0, // Pixel size of single piece
+    pieces: [], // Store objests, which have elem properties
+    puzzle: {}, // element
 
+    // Some preparation works, including clean old pieces, create new ones
+    // call when creating new game, restarting and change pictures...
     prepareGame: function() {
+        // clean old ones
         for (var i = 0; i < this.pieces.length; i++) {
             for (var j = 0; j < this.pieces.length; j++) {
                 document.removeChild(this.pieces[i][j]);
@@ -12,7 +17,7 @@ var game = {
         }
         this.pieces = [];
 
-        
+        this.getPcsSize();
 
         for (var i = 0; i < 8; i++) {
             game.blocks[i] = [];
@@ -33,31 +38,50 @@ var game = {
                     game.board_2.appendChild(elem);
                 }
             }
-            if (i === 0) {
-                for (var a = 0; a < 4; a++) {
-                    game.blocks[i][a].classList.add("info-block");
-                    game.blocks[i][a].addEventListener("click", game.selectHandler, false);
-
-                    var text = document.createTextNode(game.blocksNumIndex[a]);
-                    game.blocks[i][a].appendChild(text);
-                }
-            } else {
-                game.addBlackBlock(game.blocks[i]);
-            }
         }
-        var firstBlackBlock = game.board_1.getElementsByClassName("blackBlock")[0];
-        firstBlackBlock.appendChild(document.createTextNode("开始"));
-        firstBlackBlock.addEventListener("click", game.startHandler, false);
-
-        game.addEventListenerForLine(game.blocks[1]);
-
-        game.timer.reset();
-        game.blocksCount = 0;
-        document.getElementById("progress-bar").style.width = "0";
-        game.infoBoard.style.display = "none";
     },
 
+    // calculate size of piece by size of whole puzzle
+    getPcsSize: function() {
 
+    }
+};
+
+// Piece constructor
+function Piece(idx, posX, posY, isBlank) {
+    this.idx = idx;
+    this.isBlank = isBlank;
+
+    this.elem = document.createElement("div");
+    this.elem.classList.add("piece");
+    if (isBlank) {
+        this.elem.id = "blank";
+    }
+
+    game.puzzle.appendChild(this.elem);
+    
+    // set element size and position
+    this.elem.style.width = this.pcsSize + "px";
+    this.elem.style.height = this.pcsSize + "px";
+    this.elem.style.top = posX * this.pcsSize + "px";
+    this.elem.style.left = posY * this.pcsSize + "px";
+}
+
+Piece.prototype = {
+    constructor : Piece,
+
+    // move: function(dir) {
+    //     var newPos = Game.tetromino.calculate(dir);
+    //     if (Game.check(newPos)) {
+    //         for (var i = 0; i < 4; i++) {
+    //             Game.tetromino.blocks[i].style.top = newPos[i].top + "px";
+    //             Game.tetromino.blocks[i].style.left = newPos[i].left + "px";
+    //         }
+    //         return true;
+    //     } else {
+    //         return false;
+    //     }
+    // }
 };
 
 window.addEventListener("load", function() {
